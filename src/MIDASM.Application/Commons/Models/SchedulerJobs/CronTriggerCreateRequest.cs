ed98b5f5c3f86f173d102dda61f133bea3dc4bdb
hieu dev <hieudev623@gmail.com>
2025-05-08 23:51:52 +0700

    add validation : add validation scheduler trigger create request

--- src/MIDASM.Application/Commons/Models/SchedulerJobs/CronTriggerCreateRequest.cs
+++ src/MIDASM.Application/Commons/Models/SchedulerJobs/CronTriggerCreateRequest.cs
@@ -1,11 +1,14 @@
 ﻿
+using FluentValidation;
+using MIDASM.Contract.Messages.Validations;
+using Quartz;
+
 namespace MIDASM.Application.Commons.Models.SchedulerJobs;
 
 public class CronTriggerCreateRequest
 {
     public string JobName { get; set; } = default!;
     public string JobGroup { get; set; } = default!;
-    public string JobType { get; set; } = default!;
     public string TriggerName { get; set; } = default!;
     public string TriggerGroup { get; set; } = default!;
     public string CronExpression { get; set; } = default!;
@@ -13,4 +16,64 @@ public class CronTriggerCreateRequest
     public DateTimeOffset? EndAt { get; set; }
 
     public string? TimeZone { get; set; } = default;
+}
+public class CronTriggerCreateRequestValidator
+    : AbstractValidator<CronTriggerCreateRequest>
+{
+    public CronTriggerCreateRequestValidator()
+    {
+        RuleFor(x => x.JobName)
+            .NotEmpty().WithMessage(SchedulerValidationMessages.JobNameRequired)
+            .MaximumLength(150).WithMessage(SchedulerValidationMessages.JobNameRequiredLessThanMaxLength);
+
+        RuleFor(x => x.JobGroup)
+            .NotEmpty().WithMessage(SchedulerValidationMessages.JobGroupRequired)
+            .MaximumLength(150).WithMessage(SchedulerValidationMessages.JobGroupRequiredLessThanMaxLength);
+
+        RuleFor(x => x.TriggerName)
+            .NotEmpty().WithMessage(SchedulerValidationMessages.TriggerNameRequired)
+            .MaximumLength(150).WithMessage(SchedulerValidationMessages.TriggerNameRequiredLessThanMaxLength);
+        
+
+        RuleFor(x => x.TriggerGroup)
+            .NotEmpty().WithMessage(SchedulerValidationMessages.TriggerGroupRequired)
+            .MaximumLength(150).WithMessage(SchedulerValidationMessages.TriggerGroupRequiredLessThanMaxLength);
+
+        RuleFor(x => x.CronExpression)
+            .NotEmpty().WithMessage(SchedulerValidationMessages.CronExpressionRequired)
+            .Must(CronExpression.IsValidExpression)
+                .WithMessage(SchedulerValidationMessages.CronExpressionInvalid);
+
+        RuleFor(x => x.TimeZone)
+            .Cascade(CascadeMode.Stop)
+            .Must(BeAValidTimeZone).When(x => !string.IsNullOrWhiteSpace(x.TimeZone))
+            .WithMessage(timeZone => SchedulerValidationMessages.InvalidTimeZoneIdentifier)
+            .When(x => !string.IsNullOrEmpty(x.TimeZone));
+
+        RuleFor(x => x)
+            .Must(x =>
+            {
+                if (x.StartAt.HasValue && x.EndAt.HasValue)
+                    return x.EndAt.Value > x.StartAt.Value;
+                return true;
+            })
+            .WithMessage(SchedulerValidationMessages.StartAtMustLessThanOrEqualEndAt);
+    }
+
+    private static bool BeAValidTimeZone(string? timeZoneId)
+    {
+        try
+        {
+            TimeZoneInfo.FindSystemTimeZoneById(timeZoneId!);
+            return true;
+        }
+        catch (TimeZoneNotFoundException)
+        {
+            return false;
+        }
+        catch (InvalidTimeZoneException)
+        {
+            return false;
+        }
+    }
 }
---
+++ src/MIDASM.Contract/Messages/Validations/SchedulerValidationMessages.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MIDASM.Contract.Messages.Validations;
+
+public static class SchedulerValidationMessages
+{
+    public static string JobNameRequired => "Job name is required";
+    public static string JobGroupRequired => "Job group is required";
+    public static string CronExpressionInvalid => "CronExpression is not a valid Quartz cron expression.";
+    public static string JobNameRequiredLessThanMaxLength => "Job name must be at most 150 characters";
+    public static string JobGroupRequiredLessThanMaxLength => "Job group must be at most 150 characters";
+    public static string TriggerNameRequired => "Trigger name is required";
+    public static string TriggerGroupRequired => "Trigger group is required";
+    public static string TriggerNameRequiredLessThanMaxLength => "Trigger name must be at most 150 characters";
+    public static string TriggerGroupRequiredLessThanMaxLength => "Trigger group must be at most 150 characters";
+    public static string CronExpressionRequired => "Cron expression is required";
+    public static string InvalidTimeZoneIdentifier => "Invalid time zone identifier";
+    public static string StartAtMustLessThanOrEqualEndAt => "Start time must less than or equal end time";
+}
